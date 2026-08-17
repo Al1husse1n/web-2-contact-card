8889ea615548af3531a7bdf5116f6a93718dd2ce
Ali Hussein <ah0979075074@gmail.com>
2026-08-17 21:13:06 +0300

    initial commit

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Social from "./components/social.jsx";
+
+function ListFoods({Favourite}){
+    return(
+        <div>
+            <h1>Foods</h1>
+            <ul>
+                <li>{Favourite}</li>
+                <li>Chicken</li>
+            </ul>
+        </div>
+    )
+}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Social />
+  </StrictMode>,
+)
